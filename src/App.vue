f1dc6a236b900223b476f7f93379b8357d13593e
esther-v <esvalas@gmail.com>
2024-02-07 21:00:02 +0100

    add delete button

--- src/App.vue
+++ src/App.vue
@@ -22,15 +22,24 @@
       done: false,
       createdAt: new Date().getTime()
     })
+
+    input_content.value = ''
+    input_category.value = null
   }
+
+  const removeTodo = todo => {
+    todos.value = todos.value.filter(t => t !== todo)
+  }
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+  })
 
   watch(todos, newVal => {
     localStorage.setItem('todos', JSON.stringify(newVal))
   }, { deep: true })
 
-  watch(name, (newVal) => {
-    localStorage.setItem('name', newVal)
-  })
+  
 
   onMounted(() => (
     name.value = localStorage.getItem('name') || '',
@@ -87,11 +96,20 @@
     <section class="todo-list">
       <h3>TODO LIST</h3>
       <div class="list">
-        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+        <div v-for="todo in todos_asc" :key="todo" :class="`todo-item ${todo.done && 'done'}`">
           <label>
             <input type="checkbox" v-model="todo.done" />
             <span :class="`bubble ${todo.category}`"></span>
           </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="action">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+
         </div>
       </div>
     </section>
